03d1157a4de03e5b1b47a1ce58307c47cde7bdf2
Andres Kukk <klambrike@gmail.com>
2022-05-19 13:05:06 +0300

    Add initial react native files

---
+++ App.js
@@ -0,0 +1,38 @@
+/**
+ * Shopping List
+ * https://github.com/klambrike/shoppinglist
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
---
+++ README.md
@@ -0,0 +1,13 @@
+# Shopping List
+
+A basic React native application for managing shopping list. The data is stored in Firebase Firestore, accessed by REST API and the state in managed with Redux.
+
+## Installing
+
+Requires node.js, Android or iOs development tools and React Native CLI (npm install -g react-native-cli). More information can be found at [Environment setup](https://reactnative.dev/docs/environment-setup)
+
+1. CD to project directory and `$ npm install`
+
+2.1. On iOs `run ios/ShoppingList.xcodeproj`
+
+2.2. On Android connect a physical device to computer or start an emulator and run `npx react-native run android`
